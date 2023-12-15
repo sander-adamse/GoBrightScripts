1496f2c21dc2720568f7abefa650a1dcd081a8c2
sander-adamse <sander.adamse@outlook.com>
2023-12-15 09:36:03 +0100

    rewritten entire script added better error handling added better logging added better output

--- GoBrightUpdater.ps1
+++ GoBrightUpdater.ps1
@@ -1,55 +1,104 @@
+Import-Module -Name Microsoft.PowerShell.Security
+
+Write-Output "Checking if the user 'NC-KioskUser' is logged in..."
 $sessionInfo = quser | Where-Object { $_ -match 'NC-KioskUser' }
 if ($sessionInfo) {
-    $sessionId = $sessionInfo -split '\s+' | Select-Object -Index 2
-    logoff $sessionId
+    try {
+        $sessionId = $sessionInfo -split '\s+' | Select-Object -Index 2
+        logoff $sessionId
+        Write-Output "User 'NC-KioskUser' logged off."
+    }
+    catch {
+        Write-Output "An error occurred while logging off the user 'NC-KioskUser'."
+    }
 }
 else {
     Write-Host "User 'NC-KioskUser' not found."
 }
 
-try {
-    $process = Get-Process -Name "GoBright.Signage.Player" -ErrorAction Stop
-    $process | Stop-Process -Force
-    $process = Get-Process -Name "GoBright.Signage.Player.Bootstrapper" -ErrorAction Stop
-    $process | Stop-Process -Force
-}
-catch [Microsoft.PowerShell.Commands.ProcessCommandException] {
-    Write-Host "$process not found" 
-}
-catch {
-    Write-Host "An error occurred: $($_.Exception.Message)"
-}
-
-try {
-    Start-Process powershell -Verb RunAs -ArgumentList "-Command Remove-Item -Path 'C:\Users\sande\Downloads\test' -Recurse -Force"
-}
-catch {
-    Write-Output "An error occurred during the deletion of the folder '_dotnetbrowser-binaries'."
+Write-Output "Checking if the processes 'GoBright.Signage.Player' and 'GoBright.Signage.Player.Bootstrapper' are running..."
+$processes = "GoBright.Signage.Player", "GoBright.Signage.Player.Bootstrapper"
+if ($processes) {
+    foreach ($process in $processes) {
+        try { 
+            Get-Process -Name $process -ErrorAction Stop | Stop-Process -Force
+            Write-Output "Process '$process' stopped." 
+        }
+        catch [Microsoft.PowerShell.Commands.ProcessCommandException] { 
+            Write-Host "$process not found" 
+        } 
+        catch { Write-Host "An error occurred: $($_.Exception.Message)" }
+    }
 }
 
-try {
-    Import-Module -Name Microsoft.PowerShell.Security
-
-    $folderPath = "C:\gobright-view\"
-    $acl = Get-Acl -Path $folderPath
-    $rule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule("Gebruikers", "FullControl", "ContainerInherit,ObjectInherit", "None", "Allow")
-    $acl.SetAccessRule($rule)
-    Set-Acl -Path $folderPath -AclObject $acl
+Write-Output "Checking if the folder '_dotnetbrowser-binaries' exists..."
+$folderPath = "C:\gobright-view\_dotnetbrowser-binaries"
+if (Test-Path -Path $folderPath) {
+    try {
+        Start-Process powershell -Verb RunAs -ArgumentList "-Command Remove-Item -Path '$folderPath' -Recurse -Force"
+        Write-Output "Folder '$folderPath' deleted."
+    }
+    catch {
+        Write-Output "An error occurred during the deletion of the folder '_dotnetbrowser-binaries'."
+    }
 }
-catch {
-    Write-Output "An error occured while setting permissions on the folder 'C:\gobright-view\'"
+else {
+    Write-Host "Folder '$folderPath' does not exist."
 }
 
-Write-Output 'Downloading the latest update and put it in the install folder'
-    
-#URL to GoBright Installer | https://install.gobright.cloud/view/windows/?mode=download&version=5.8.9 ---> Current version used
-$Url = 'http://install.gobright.cloud/view/windows/latest'
+Write-Output "Checking if the folder 'C:\gobright-view\' exists..."
+$folderPath = "C:\gobright-view\"
+if (Test-Path -Path $folderPath) {
+    try {
+        $acl = Get-Acl -Path $folderPath
+        $rule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule("Gebruikers", "FullControl", "ContainerInherit,ObjectInherit", "None", "Allow")
+        $acl.SetAccessRule($rule)
+        Set-Acl -Path $folderPath -AclObject $acl
+        Write-Output "Permissions set on folder '$folderPath'."
+    }
+    catch {
+        Write-Output "An error occured while setting permissions on the folder 'C:\gobright-view\'"
+    }
+}
+else {
+    Write-Host "Folder '$folderPath' does not exist."
+}
 
-Invoke-WebRequest -Uri $Url -OutFile "C:\gobright-view\download.zip"
-Rename-Item -Path "C:\gobright-view\download.zip" -NewName "C:\gobright-view\update.zip"
-Start-Process -WorkingDirectory "C:\gobright-view\bootstrapper\" -FilePath "GoBright.Signage.Player.Bootstrapper.exe"
+$HTTP_Request = [System.Net.WebRequest]::Create('http://install.gobright.cloud/view/windows/latest')
+$HTTP_Response = $HTTP_Request.GetResponse()
+$HTTP_Status = [int]$HTTP_Response.StatusCode
+
+if ($HTTP_Status -eq 200) {
+    #URL to GoBright Installer | https://install.gobright.cloud/view/windows/?mode=download&version=5.8.9 ---> Current version used
+    Write-Output 'Downloading the latest update and put it in the install folder' 
+    $Url = 'http://install.gobright.cloud/view/windows/latest'
+    Invoke-WebRequest -Uri $Url -OutFile "C:\gobright-view\download.zip"
+    Rename-Item -Path "C:\gobright-view\download.zip" -NewName "C:\gobright-view\update.zip"
+
+    If ($HTTP_Response -eq $null) { } 
+    Else { $HTTP_Response.Close() }
+}
+else {
+    Write-Host "The Site may be down, please check."
+}
+
+$filePath = "C:\gobright-view\bootstrapper\GoBright.Signage.Player.Bootstrapper.exe"
+if (Test-Path -Path $filePath) {
+    Write-Output "Starting the GoBright.Signage.Player.Bootstrapper.exe..."
+    Start-Process -WorkingDirectory "C:\gobright-view\bootstrapper\" -FilePath "GoBright.Signage.Player.Bootstrapper.exe"
+}
+else {
+    Write-Host "File '$filePath' does not exist."
+}
 
 $delayInSeconds = 300  # 5 minutes in seconds
 Write-Host "Restarting computer in $delayInSeconds seconds..."
-Start-Sleep -Seconds $delayInSeconds
-Restart-Computer -Force
+try {
+    Start-Sleep -Seconds $delayInSeconds
+    Restart-Computer -Force
+}
+catch {
+    Write-Output "An error occurred while restarting the computer."
+}
+
+
